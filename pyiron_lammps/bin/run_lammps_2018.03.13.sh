a3bae320d862dde004b38c7032f845f695d6216e
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-16 16:07:14 +0200

    Update run_lammps_2018.03.13.sh

--- pyiron_lammps/bin/run_lammps_2018.03.13.sh
+++ pyiron_lammps/bin/run_lammps_2018.03.13.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-if $(command -v $lmp); then 
-    lmp -in control.inp; 
-elif  $(command -v $lmp_serial); then 
-    lmp_serial -in control.inp;
-else: 
-    echo "No Lammps executable found using $0"
-fi;
+lmp_serial -in control.inp;
