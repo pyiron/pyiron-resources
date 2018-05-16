addbddebef32d67b7a73989cb64a7fef036d3eaf
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-16 16:22:25 +0200

    Update run_lammps_2018.03.13_mpi.sh

--- pyiron_lammps/bin/run_lammps_2018.03.13_mpi.sh
+++ pyiron_lammps/bin/run_lammps_2018.03.13_mpi.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-if $(command -v $lmp); then 
-    mpiexec -n $1 lmp -in control.inp; 
-elif  $(command -v $lmp_mpi); then 
-    mpiexec -n $1 lmp_mpi -in control.inp;
-else 
-    echo "No Lammps executable found using $0"
-fi;
+mpiexec -n $1 lmp_mpi -in control.inp;
