e3ea4c195eb9ca4b074982d2af4f9c89213ac487
Alexander Knoll <aknoll@gwdg.de>
2022-05-25 14:28:46 +0200

    Add new resources for RuNNer based on the pyiron interface.

--- runner/bin/run_runner_1.2.sh
+++ runner/bin/run_runner_1.2.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-RuNNer.serial.x
-sed -i 's/runner_mode 1/runner_mode 2/g' input.nn
-RuNNer.serial.x
-sed -i 's/runner_mode 2/runner_mode 3/g' input.nn
-OLD=$(ls optweights.*.out)
-TMP=${OLD/opt/}
-mv $OLD ${TMP/out/data}
+mode=$(awk '/runner_mode/{print $2}'  input.nn)
+RuNNer.serial.x | tee mode${mode}.out
